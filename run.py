42db2feb48d368b0785bb6353a6aeedfb6ef20cf
Porodin <creshe2001@mail.ru>
2023-10-20 13:56:53 +0300

    modified buttons

--- run.py
+++ run.py
@@ -1,13 +1,16 @@
 import asyncio
+import requests
 
-from aiogram import Bot, Dispatcher, types, F
+from aiogram import Bot
+from aiogram import Dispatcher, types, F
 from aiogram.filters import CommandStart
 from aiogram.types import Message, CallbackQuery
+from bs4 import BeautifulSoup
+from selenium.webdriver.chrome import webdriver
 
 Token_API = "6645247048:AAFYx8UJ9A8oDyFnBVHbJ8uVntpBTyBYNEE"
 bot = Bot(Token_API)
 dp = Dispatcher()
-
 
 @dp.message(CommandStart())
 async def command_start_handler(message: Message):
@@ -15,11 +18,12 @@ async def command_start_handler(message: Message):
         f'Приветствую тебя, {message.from_user.first_name}! Я бот, который позволит тебе выбрать товар, по интересующей тебя категории в таких интернет-магазинах как:<b> <a href="https://www.sportmaster.ru"> Спортмастер</a></b>, <b> <a href="https://www.ozon.ru">Ozon</a></b>, <b> <a href="https://www.wildberries.ru">Wildberries</a></b>',
         parse_mode="HTML", disable_web_page_preview=True)
 
-    firstb = types.InlineKeyboardButton(text='🙂да', callback_data='yes')
-    secondb = types.InlineKeyboardButton(text='😔нет', callback_data='no')
-    ikb = types.InlineKeyboardMarkup(inline_keyboard=[[firstb], [secondb]], row_width=2)
-
     await bot.send_message(chat_id=message.from_user.id, text='Открыть список категорий?', reply_markup=ikb)
+
+
+firstb = types.InlineKeyboardButton(text='🙂да', callback_data='yes')
+secondb = types.InlineKeyboardButton(text='😔нет', callback_data='no')
+ikb = types.InlineKeyboardMarkup(inline_keyboard=[[firstb], [secondb]], row_width=2)
 
 
 @dp.callback_query(F.data == 'yes')
@@ -40,11 +44,30 @@ async def process_button_1_press(callback_query: types.CallbackQuery):
         )
         await callback_query.message.answer('Список всех категорий...', reply_markup=keyboard)
 
+
 @dp.callback_query(F.data == 'no')
 async def process_button_2_press(callback_query: CallbackQuery):
     if callback_query.message.text != 'no':
         await callback_query.message.answer(
             text='Значит в другой раз')
+
+
+@dp.message(F.text == 'гантели')
+async def collback_inline(qalback: CallbackQuery):
+    link = "https://www.sportmaster.ru/"
+    url = "https://www.sportmaster.ru/catalog/vidy_sporta_/trening/svobodnyy_ves/"
+    chat_id = qalback.chat.id
+    page = requests.get(url)
+    print(page.status_code)
+    soup = BeautifulSoup(page.text, 'lxml')
+    section = soup.find_all("div", class_="sm-product-grid sm-product-grid--size-xs")
+    for products in section:
+        product = products.find_all("div", class_="sm-product-card sm-product-grid__product")
+        for item in product:
+            product_name = item.find("a", class_="sm-link sm-link_black").get_text(strip=True)
+            product_price = item.find("div", class_="price hvr-sweep-to-right").get_text(strip=True)
+            product_link = link + item.find("a", class_="product_list_img").get("href")
+            all_poducts = f"{product_name}\n Цена: {product_price}\n Ссылка: {product_link}"
 
 
 async def main() -> None:
